0383949eb73e63a0d27de9bffc647c021da47940
Wataru Sato <wataru@rubyquitous.co.jp>
2014-07-27 00:21:48 +0900

    itemのデザイン

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,4 +6,12 @@ class TopController < ApplicationController
       render "index", layout: "before_authentication"
     end
   end
+  
+  def search
+    if current_user
+      render "search", layout: "application"
+    else
+      render "search", layout: "before_authentication"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+  get 'search', to: 'top#search'
   resource :session, only: [ :new, :create, :destroy ]
+  resources :items do
+    get :search, on: :collection
+  end
 end
